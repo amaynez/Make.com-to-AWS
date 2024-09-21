60689911aa02baf0e39b418a6a2c3f633f4157fa
Armando Maynez <armando.maynez@gmail.com>
2024-09-21 11:26:13 -0600

    fixed premissions with new S3 bucket created from the SAM template

--- VBliss_Blog/.aws-sam/cache/968ef099-d35b-4c2b-88f9-865bd9652ab0/Concatenate_Results.py
+++ VBliss_Blog/.aws-sam/cache/968ef099-d35b-4c2b-88f9-865bd9652ab0/Concatenate_Results.py
@@ -2,43 +2,22 @@ import re
 import json
 
 def trim_before_heading(text):
-    # Try to find <h1> first
-    h1_match = re.search(r'<h1>', text, re.IGNORECASE)
-    if h1_match:
-        return text[h1_match.start():]
-    
-    # If <h1> is not found, look for <h2>
-    h2_match = re.search(r'<h2>', text, re.IGNORECASE)
-    if h2_match:
-        return text[h2_match.start():]
-    
-    # If neither <h1> nor <h2> is found, return the original text
-    return text
+    match = re.search(r'<h[12]>', text, re.IGNORECASE)
+    return text[match.start():] if match else text
 
 def lambda_handler(event, context):
-    # The event will contain an array of all processed sections
-    processed_sections = event
+    blog_sections = []
     
-    blog_post = ""
+    for section in event:
+        body_content = json.loads(section.get('body', '{}'))
+        blog_section = body_content.get('blog_section', '')
+        blog_sections.append(trim_before_heading(blog_section))
     
-    # Concatenate all processed sections
-    for section in processed_sections:
-        # Assuming each section is a dictionary with a 'body' key
-        body_content = section.get('body', '{}')
-        body_content_post = json.loads(body_content)
-        blog_section = body_content_post.get('blog_section', '')
-        blog_post += trim_before_heading(blog_section) + "\n\n"
-    
-    # Remove any extra newlines
-    blog_post = blog_post.strip()
-    
-    # Remove all <h1> tags and their content
+    blog_post = '\n\n'.join(blog_sections).strip()
     blog_post = re.sub(r'<h1>.*?</h1>', '', blog_post, flags=re.IGNORECASE | re.DOTALL)
 
     return {
         'statusCode': 200,
-        'body': json.dumps({
-            'blog_post': blog_post
-        })
+        'body': json.dumps({'blog_post': blog_post})
     }
 
